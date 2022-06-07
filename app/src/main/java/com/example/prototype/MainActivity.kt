52c75c679022fb7232c0be8decd853e2edcb001b
Qiran2022 <qil68@pitt.edu>
2022-06-06 23:40:41 -0400

    test

--- app/src/main/java/com/example/prototype/MainActivity.kt
+++ app/src/main/java/com/example/prototype/MainActivity.kt
@@ -8,4 +8,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+    fun sendMessage(view: View) {
+        // Do something in response to button
+    }
 }
